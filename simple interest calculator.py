a6944250e16420c4172270b3717b6f36fe35ec42
Purushottam Malgi <46858778+purumalgi@users.noreply.github.com>
2020-09-04 00:41:10 +0530

    Add files via upload

---
+++ simple interest calculator.py
@@ -0,0 +1,10 @@
+P = float(input("Enter Principal Amount: "))
+R = float(input("Enter the Rate: "))
+T = float(input("Enter the time period: "))
+
+SI = (P*R*T)/100
+
+TA = P+SI
+
+print("Total amount to be paid is: ", TA)
+
